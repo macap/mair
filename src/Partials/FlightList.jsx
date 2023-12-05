2c06385729360f9d5665f9a2bfbeb67ad90c91cf
Maciek Caputa <8134011+macap@users.noreply.github.com>
2023-12-05 10:42:19 +0100

    Adds small fix for content shift in layout on smaller screens

--- src/Partials/FlightList.jsx
+++ src/Partials/FlightList.jsx
@@ -4,43 +4,41 @@ import FlightListElement from "../Components/FlightListElement";
 
 function FlightList({ data, onSelect, currentDate }) {
   return (
-    <div style={{ maxWidth: "600px" }}>
-      <ExpandableList>
-        {data.fares.map((el) => {
-          const disabled = isBefore(
-            new Date(el.outbound.departureDate),
-            currentDate
-          );
-          const [departureDate, departureTime] =
-            el.outbound.departureDate.split("T");
-          const [arrivalDate, arrivalTime] = el.outbound.arrivalDate.split("T");
+    <ExpandableList>
+      {data.fares.map((el) => {
+        const disabled = isBefore(
+          new Date(el.outbound.departureDate),
+          currentDate
+        );
+        const [departureDate, departureTime] =
+          el.outbound.departureDate.split("T");
+        const [arrivalDate, arrivalTime] = el.outbound.arrivalDate.split("T");
 
-          const duration = "??h ??min";
+        const duration = "??h ??min";
 
-          return (
-            <FlightListElement
-              departureDate={departureDate}
-              departureTime={departureTime}
-              departureAirportName={el.outbound.departureAirport.name}
-              departureAirportCode={el.outbound.departureAirport.iataCode}
-              departureCountryName={el.outbound.departureAirport.countryName}
-              arrivalDate={arrivalDate}
-              arrivalTime={arrivalTime}
-              arrivalAirportName={el.outbound.arrivalAirport.name}
-              arrivalAirportCode={el.outbound.arrivalAirport.iataCode}
-              arrivalCountryName={el.outbound.arrivalAirport.countryName}
-              number={el.outbound.flightNumber}
-              duration={duration}
-              priceValue={el.summary.price.value}
-              currencySymbol={el.summary.price.currencySymbol}
-              key={el.outbound.arrivalAirport.iataCode}
-              isDisabled={disabled}
-              onClick={disabled ? () => null : () => onSelect(el)}
-            />
-          );
-        })}
-      </ExpandableList>
-    </div>
+        return (
+          <FlightListElement
+            departureDate={departureDate}
+            departureTime={departureTime}
+            departureAirportName={el.outbound.departureAirport.name}
+            departureAirportCode={el.outbound.departureAirport.iataCode}
+            departureCountryName={el.outbound.departureAirport.countryName}
+            arrivalDate={arrivalDate}
+            arrivalTime={arrivalTime}
+            arrivalAirportName={el.outbound.arrivalAirport.name}
+            arrivalAirportCode={el.outbound.arrivalAirport.iataCode}
+            arrivalCountryName={el.outbound.arrivalAirport.countryName}
+            number={el.outbound.flightNumber}
+            duration={duration}
+            priceValue={el.summary.price.value}
+            currencySymbol={el.summary.price.currencySymbol}
+            key={el.outbound.arrivalAirport.iataCode}
+            isDisabled={disabled}
+            onClick={disabled ? () => null : () => onSelect(el)}
+          />
+        );
+      })}
+    </ExpandableList>
   );
 }
 
